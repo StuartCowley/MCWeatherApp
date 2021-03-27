2d9cea016c6f2ac11c727eb5e3634b820c37989a
StuartCowley <voidstalker@live.co.uk>
2021-03-27 19:09:05 +0000

    added dev dependencies and eslint settings

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders weather app heading', () => {
+test("renders weather app heading", () => {
   render(<App />);
   const linkElement = screen.getByText(/weather app/i);
   expect(linkElement).toBeInTheDocument();
