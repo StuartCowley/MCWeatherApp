35e161714bbf9995535e09bbd97a5cf43ee5d999
StuartCowley <voidstalker@live.co.uk>
2021-03-28 23:35:41 +0100

    fixed date and temperature formats

--- src/components/ForecastSummary.js
+++ src/components/ForecastSummary.js
@@ -3,12 +3,25 @@ import PropTypes from "prop-types";
 import "../styles/App.css";
 
 function ForecastSummary({ date, icon, temperature, description }) {
+  // Create date in format `shortWeekDay numericDay shortMonth` for local language (hard coded to GB for now)
+  const locale = "en-GB";
+  const options = {
+    month: "short",
+    day: "numeric",
+  };
+  // As toLocaleDateString() includes a comma after the day by default,
+  // we have to get the day first then append the numeric day and month
+  let formattedDate = new Date(date).toLocaleDateString(locale, {
+    weekday: "short",
+  });
+  formattedDate += ` ${new Date(date).toLocaleDateString(locale, options)}`;
+
   return (
     <div className="forecast-summary">
-      <h1>forecast summary 1</h1>
-      <div>{date}</div>
+      <h2>Forecast summary</h2>
+      <div>{formattedDate}</div>
       <div>{icon}</div>
-      <div>{temperature.max}</div>
+      <div>{`${temperature.max}\u2103`}</div>
       <div>{description}</div>
     </div>
   );
